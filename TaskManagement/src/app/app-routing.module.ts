651117801a7d0ad2b7e58fa5aa042b106d2f4efc
Gopikrishna Boda <gopikrishna.boda@solugenix.com>
2022-05-10 13:32:46 +0530

    modified routing paths

--- TaskManagement/src/app/app-routing.module.ts
+++ TaskManagement/src/app/app-routing.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreateTaskComponent } from './create-task/create-task.component';
+
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import { TaskListComponent } from './task-list/task-list.component';
+
 
 const routes: Routes = [{path:'signup',component : SignupComponent},
 {path:'login',component : LoginComponent},
 { path: 'index', loadChildren: () => import('./index/index.module').then(m => m.IndexModule) },
 {path:'signup',component : SignupComponent},
-{ path: 'create-task', component: CreateTaskComponent },
-{ path: 'task-list', component: TaskListComponent},
-// {path: 'create-task', component: CreateTaskComponent}
+
 ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TaskManagement/src/app/index/index.module.ts
+++ TaskManagement/src/app/index/index.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { IndexRoutingModule } from './index-routing.module';
 import { IndexComponent } from './index.component';
-import { CreateTaskComponent } from '../create-task/create-task.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
 import { ReactiveFormsModule } from '@angular/forms';
 // import { TaskListComponent } from '../task-list/task-list.component';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
